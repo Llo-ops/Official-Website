196360181dfd43679b36f446d679439c368cb3d6
Llo-ops <loopsalo28@gmail.com>
2025-11-14 10:41:53 +0800

    Fixed Style Complications with flexbox

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -3,7 +3,43 @@ import '@/app/sass/main/style.scss';
 export default function HomePage() {
   return(
     <>
-      <h1>This is the Home page</h1>
+      <div className="home-page-layout">
+        <section className="top-portion">
+          <div className="text-top-portion">
+            <h1>Welcome to my Website Portfolio</h1>
+            <p>
+              I design and conceptualize stories brewed into a perfect balance of design,
+              <br />
+              code, and bringing your imaginations to life.
+              <br />
+              <br />
+              So sit back, relax, grab a coffee, and enjoy exploring as much as you can.
+            </p>
+          </div>
+
+          <div className="intro-frame">
+            <div className="intro-title">
+              <p>WHAT I DO</p>
+            </div>
+            <div className="intro-cards">
+              <span>1</span>
+              <span>2</span>
+              <span>3</span>
+            </div>
+          </div>
+        </section>
+
+        <section className="bottom-portion">
+          <div className="socials">
+            <span>1</span>
+            <span>2</span>
+            <span>3</span>
+          </div>
+          <button className="contact-quick-selection">
+            <p>CONTACT ME</p>
+          </button>
+        </section>
+      </div>
     </>
   )
 }
--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Link from 'next/link';
+import { useState } from 'react';
 import '@/app/sass/main/style.scss';
 
 export default function PagesLayout({
@@ -9,23 +10,51 @@ export default function PagesLayout({
   children : React.ReactNode,
 }) {
   const navigationLinks = [
-    {name: "Home", href: "/home"},
-    {name: "Projects", href: "/projects"},
-    {name: "About", href: "/about"},
-    {name: "Contact", href: "/contact"},
+    {name: "HOME", href: "/home"},
+    {name: "PROJECTS", href: "/projects"},
+    {name: "ABOUT", href: "/about"},
+    {name: "CONTACT", href: "/contact"},
   ]
   
+  const [open, setOpen] = useState(false);
+
   return (
     <html>
       <body>
-        <div className='nav-header'>
-          {navigationLinks.map((link) => {
-            return (
-              <Link href={link.href} key={link.name}>{link.name}</Link>
-            )
-          })}
+        <div className="layout-flex">
+          <section className="header-frame">
+            <div className='navbar'>
+              {navigationLinks.map((link) => {
+                return (
+                  <Link
+                    className='nav-text-style'
+                    href={link.href} key={link.name}>
+                      {link.name}
+                  </Link>
+                )
+              })}
+            </div>
+          </section>
+          <div className='children-page'>{children}</div>
+
+          <section className="footer-frame">
+            <div className="footer-year">
+              <p>· 2025 ·</p>
+            </div>
+            <div className="footer-spacer">
+
+            </div>
+            <div className="footer-right-group">
+              <div className="name">
+                <p>GEROME GEISLER TEJIDA</p>
+              </div>
+              <span>·</span>
+              <div className="credits">
+                <button>CREDITS</button>
+              </div>
+            </div>
+          </section>
         </div>
-        <div>{children}</div>
       </body>
     </html>
   );
