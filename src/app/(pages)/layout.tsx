9673ee96f0886961e363edcbf4a5767145c06809
Llo-ops <loopsalo28@gmail.com>
2025-11-15 08:34:21 +0800

    Added Additional Styles and Changes to the layout and design of specific pages

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -2,13 +2,24 @@
 
 import Link from 'next/link';
 import { useState } from 'react';
+import { usePathname } from 'next/navigation';
 import '@/app/sass/main/style.scss';
+
+import { 
+  FacebookIcon, 
+  GithubIcon, 
+  LinkedinIcon, 
+  Phone, 
+  ArrowRightToLine } from 'lucide-react';
 
 export default function PagesLayout({
   children,
 }: {
   children : React.ReactNode,
 }) {
+
+  const pathname = usePathname();
+
   const navigationLinks = [
     {name: "HOME", href: "/home"},
     {name: "PROJECTS", href: "/projects"},
@@ -21,7 +32,7 @@ export default function PagesLayout({
   return (
     <html>
       <body>
-        <div className="layout-flex">
+        <div className="layout-flex-wrapper">
           <section className="header-frame">
             <div className='navbar'>
               {navigationLinks.map((link) => {
@@ -35,7 +46,36 @@ export default function PagesLayout({
               })}
             </div>
           </section>
-          <div className='children-page'>{children}</div>
+
+          {pathname === "/home" ? (
+            <section className="mid-section-home">
+              {children}
+              <div className="bottom-portion-home">
+                <button>
+                  <Phone size={24} />
+                  <span>Contact Me</span>
+                </button>
+                <div className="socials-home">
+                  <button>
+                    <FacebookIcon size={24}/>
+                    <span>Facebook</span>
+                  </button>
+                  <button>
+                    <GithubIcon size={24}/>
+                    <span>Github</span>
+                  </button>
+                  <button>
+                    <LinkedinIcon size={24}/>
+                    <span>LinkedIn</span>
+                  </button>
+                </div>
+              </div>
+            </section>
+          ): (
+            <div className="mid-section">
+              {children}
+            </div>
+          )}
 
           <section className="footer-frame">
             <div className="footer-year">
@@ -46,7 +86,7 @@ export default function PagesLayout({
             </div>
             <div className="footer-right-group">
               <div className="name">
-                <p>GEROME GEISLER TEJIDA</p>
+                <p>NAME</p>
               </div>
               <span>·</span>
               <div className="credits">
